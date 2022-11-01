c52522979c0fc64cf8b2d01c62e4decc7300ed65
HursitTarcan <HursitTarcan@gmail.com>
2022-11-01 21:33:56 +0100

    challenge three partial

--- algorithm/algorithm-one/index.js
+++ algorithm/algorithm-one/index.js
@@ -10,3 +10,20 @@ function search(nums, target) {
 }
 //Runtime: 118ms
 //Memory: 46.3MB
+
+//Challenge three
+function searchInsert(nums, target) {
+    let result = -1;
+    for (let i in nums) {
+        console.log(nums[i]);
+        if (target > nums[nums.length - 1]) {
+            result = nums.indexOf(nums[nums.length - 1]) + 1;
+        }
+    }
+    return result;
+}
+
+let arr = [2, 3, 8, 10, 11];
+console.log(searchInsert(arr, 20));
+
+//console.log(arr[arr.length - 1]);
