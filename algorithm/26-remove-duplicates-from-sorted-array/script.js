78965401adc335e34592a23e8d1184cf204eadd7
hursittarcan <hursittarcan@gmail.com>
2023-06-14 13:27:06 +0200

    start ex 26 algo

---
+++ algorithm/26-remove-duplicates-from-sorted-array/script.js
@@ -0,0 +1,9 @@
+function removeDuplicates(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+    }
+}
+
+let nums = [1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 10, 14, 15];
+
+removeDuplicates(nums);
