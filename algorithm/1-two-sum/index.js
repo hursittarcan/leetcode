ca1774f6661f5dbb4ec0cf54b639087e2c8a6732
hursittarcan <hursittarcan@gmail.com>
2023-08-18 16:20:35 +0300

    solution accepted

--- algorithm/1-two-sum/index.js
+++ algorithm/1-two-sum/index.js
@@ -1,13 +1,9 @@
 twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (nu[i] + nu[j] === target) {
-                console.log('HIT!, numbers: ' + nu[i] + ' + ' + nu[j]);
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
             }
         }
     }
 };
-
-const nu = [1, 2, 3, 4, 5];
-
-twoSum(nu, 8)
