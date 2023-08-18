ac39853fd88b9f29942257d9716cf262d7402422
hursittarcan <hursittarcan@gmail.com>
2023-08-18 16:22:17 +0300

    refactor code - exercise finished

--- algorithm/1-two-sum/index.js
+++ algorithm/1-two-sum/index.js
@@ -1,3 +1,8 @@
+//1. Two Sum - Easy
+
+//Runtime: 82ms (Beats 50.42%)
+//Memory: 42.06 (Beats 82.92%)
+
 twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
