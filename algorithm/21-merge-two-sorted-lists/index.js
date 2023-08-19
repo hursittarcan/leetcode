372eab4ff019d6e1e72563af50379e9091f86084
hursittarcan <hursittarcan@gmail.com>
2023-08-19 13:55:07 +0300

    in progress...

--- algorithm/21-merge-two-sorted-lists/index.js
+++ algorithm/21-merge-two-sorted-lists/index.js
@@ -4,20 +4,22 @@
 
 
 mergeTwoLists = function(list1, list2) {
-    let result = '';
-    let concat = list1.concat(list2);
-    console.log(concat);
 
-    let x = concat[0];
-    for (let i = 0; i < concat.length; i++) {
+    let concat = list1.concat(list2);
+    let newArr = [];
+    let number = concat[0];
+    for (let i = concat.length - 1; i >= 0; i--) {
+        if (concat)
         console.log(concat[i]);
     }
+
 };
 
 let list1 = [1,2,4];
 let list2 = [1,3,4];
 
 mergeTwoLists(list1, list2);
+
 
 // Input: list1 = [1,2,4], list2 = [1,3,4]
 // Output: [1,1,2,3,4,4]
